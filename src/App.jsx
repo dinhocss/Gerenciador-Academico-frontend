8f52e6ce3a2e863f4b2e88316952b87aef5e46b7
dinhocss <caljunior@id.uff.br>
2025-12-19 15:23:41 -0300

    Configurando novamente

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-red-500 text-white text-4xl p-10 font-bold text-center">
+      Se ficar vermelho, o Tailwind v4 funcionou! 🚀
+    </div>
+  )
+}
+
+export default App
